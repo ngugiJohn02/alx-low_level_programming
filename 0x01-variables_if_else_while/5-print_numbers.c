024f4f4336add02ee288324aa968beb65d7182b9
ngugiJohn02 <ngugijohn728@gmail.com>
2024-06-24 07:00:21 -0700

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-
 /**
-* main - Entry point
+* main - Entry point of the program
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-int digit;
-
-for (digit = 0; digit <= 9; digit++)
+char digit;
+for (digit = '0'; digit <= '9'; digit++)
 {
-putchar(digit + '0');
+putchar(digit);
 }
-    
 putchar('\n');
-
 return (0);
 }
