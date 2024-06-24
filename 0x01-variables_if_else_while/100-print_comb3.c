e0a9bbe445ba17d25c8d24cade351a271e8f5998
ngugiJohn02 <ngugijohn728@gmail.com>
2024-06-24 08:22:35 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,8 +14,8 @@ for (digit1 = 0; digit1 < 9; digit1++)
 {
 for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
-putchar((digit1 % 10) + '0');
-putchar((digit2 % 10) + '0');
+putchar(digit1 + '0');
+putchar(digit2 + '0');
 
 if (digit1 == 8 && digit2 == 9)
 continue;
@@ -25,6 +25,7 @@ putchar(' ');
 }
 
 putchar('\n');
-	
+
 return (0);
 }
+
