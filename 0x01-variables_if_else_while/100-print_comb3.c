1e82c3f24f188a31e2bcf96578f138bf5abb6d48
ngugiJohn02 <ngugijohn728@gmail.com>
2024-06-24 08:07:11 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+putchar('0');
+putchar('1');
+putchar(',');
+putchar(' ');
+putchar('0');
+putchar('2');
+putchar(',');
+putchar(' ');
+putchar('0');
+putchar('3');
+putchar(',');
+putchar(' ');
+putchar('0');
+putchar('4');
+putchar(',');
+putchar(' ');
+putchar('0');
+putchar('5');
+putchar(',');
+putchar(' ');
+putchar('\n');
+
+return 0;
+}
+
