412013500c642a1a8b540be3f9c5addb05f829e9
ngugiJohn02 <ngugijohn728@gmail.com>
2024-06-27 13:32:18 -0700

    Like, comment, subscribe

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,8 +2,8 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Prints a random number and states whether
- *        it is positive, negative, or zero.
+ * main- Entry point - 
+ * *        .
  *
  * Return: Always 0.
 */
