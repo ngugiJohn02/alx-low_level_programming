e8c3c99cdd07d2700c264265291cb0be4a70d6af
ngugiJohn02 <ngugijohn728@gmail.com>
2024-07-02 05:22:46 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
-void swap_int(int *a, int *b) {
+/**
+* swap_int - swaps the value of int a and int b
+* @a: first int
+* @b: second int
+* Return 0
+*/
+void swap_int(int *a, int *b)
+{
 int temp = *a;
 *a = *b;
 *b = temp;
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-
-int _strlen(char *s) {
+/**
+* _strlen - Returns the length of a string.
+* @s: The string to measure.
+*
+* Return: The length of the string.
+*/
+int _strlen(char *s)
+{
 int length = 0;
-while (s[length] != '\0') {
+while (s[length] != '\0')
 length++;
-}
-return length;
+return (length);
 }
