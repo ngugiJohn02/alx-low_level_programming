9548a00612bcb344f4dc00dc6f2dc540c4ab8579
ngugiJohn02 <ngugijohn728@gmail.com>
2024-11-28 01:22:39 -0800

    MAKEFILE in JUJA

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,12 @@
+#include "m.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 
+ */
+void print_school(void);
+int main(void)
+{
+    print_school();
+    return (EXIT_SUCCESS);
+}
---
+++ 0x1C-makefiles/school.c
@@ -0,0 +1,12 @@
+#include "m.h"
+
+/**
+ * print_holberton - print the logo of Holberton
+ *
+ * Return: Nothing
+ *
+ */
+void print_school(void)
+{
+    printf("Hello, School!\n");
+}
